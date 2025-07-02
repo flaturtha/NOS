1afb57ab6ab1b6e84422ad22596b5e3593971f0c
Flaturtha <richthewriterguy@gmail.com>
2025-07-02 13:14:29 -0500

    revert vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { vercelViteRemix } from "@vercel/remix/vite";
+import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -10,7 +10,15 @@ declare module "@remix-run/node" {
 
 export default defineConfig({
   plugins: [
-    vercelViteRemix(),
+    remix({
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+        v3_singleFetch: true,
+        v3_lazyRouteDiscovery: true,
+      },
+    }),
     tsconfigPaths(),
   ],
 });
