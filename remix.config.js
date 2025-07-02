ccf9447906382525598aae29a1643386adef1e24
Rich Cook <flaturtha@users.noreply.github.com>
2025-07-02 18:55:01 -0500

    (RE)Create remix.config.js

---
+++ remix.config.js
@@ -0,0 +1,8 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+module.exports = {
+  future: {
+    v3_routeConvention: true,
+  },
+  ignoredRouteFiles: ["**/.*"],
+  serverBuildTarget: "vercel",
+};
