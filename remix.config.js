74bec2075bfba50ae99c825b06a067f16e639514
Flaturtha <richthewriterguy@gmail.com>
2025-07-02 15:12:34 -0500

    more bs

--- remix.config.js
+++ remix.config.js
@@ -3,6 +3,7 @@ import { config } from "@netlify/remix-adapter";
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
   ...config,
+  server: "./server.js",
   ignoredRouteFiles: ["**/.*"],
   future: {
     v2_routeConvention: true,
