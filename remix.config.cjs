8b7b0f642a17650a5c2a16a76ffe192e12093ae1
Rich Cook <flaturtha@users.noreply.github.com>
2025-07-02 20:18:21 -0500

    Update remix.config.cjs

--- remix.config.cjs
+++ remix.config.cjs
@@ -1,10 +1,9 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  server: './server.ts',
-  ignoredRouteFiles: ['**/.*'],
-  appDirectory: 'app',
-  assetsBuildDirectory: 'public/build',
-  publicPath: '/build/',
-  serverBuildPath: 'api/index.js',
-  serverModuleFormat: 'cjs',
+  future: {
+    v3_routeConvention: true,
+  },
+  ignoredRouteFiles: ["**/.*"],
+  serverBuildTarget: "vercel",
+  server: "./entry.vercel.ts"
 };
