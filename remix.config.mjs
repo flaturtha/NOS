318b68a141e2971f9ce764e6a47f0d9faa3b81e9
Rich Cook <flaturtha@users.noreply.github.com>
2025-07-02 20:57:43 -0500

    Update remix.config.mjs

--- remix.config.mjs
+++ remix.config.mjs
@@ -1,7 +1,14 @@
-import pkg from "@vercel/remix";
-const { vercelPreset } = pkg;
-
 /** @type {import('@remix-run/dev').AppConfig} */
-export default {
-  ...vercelPreset(),
+module.exports = {
+  future: {
+    // enable static export
+    unstable_staticExport: true,
+  },
+  routes: async (defineRoutes) =>
+    defineRoutes((route) => {
+      // your index route
+      route("/", "app/routes/_index.tsx");
+      // dynamic slug route (file: app/routes/$variant.tsx)
+      route("/:variant", "app/routes/$variant.tsx");
+    }),
 };
