08f2c2e9d9938be619e26d4f28d379aa60ace11f
Rich Cook <flaturtha@users.noreply.github.com>
2025-07-02 21:03:10 -0500

    Update remix.config.cjs

--- remix.config.cjs
+++ remix.config.cjs
@@ -1,11 +1,7 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
+  appDirectory: "app",               // points at your /app folder
   future: {
-    unstable_staticExport: true,
-  },
-  routes: async (defineRoutes) =>
-    defineRoutes((route) => {
-      route("/", "app/routes/_index.tsx");
-      route("/:variant", "app/routes/$variant.tsx");
-    }),
+    unstable_staticExport: true      // enable fully static export
+  }
 };
