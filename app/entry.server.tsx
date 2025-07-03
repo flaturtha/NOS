b44b0566529ac74fa472c65654e18cec5c2f336d
Rich Cook <flaturtha@users.noreply.github.com>
2025-07-02 19:10:47 -0500

    Update entry.server.tsx

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,4 +1,4 @@
 import { createRequestHandler } from "@remix-run/vercel";
-import * as build from "@remix-run/dev/server-build";
+import * as build from "../build/index.js"; // not from @remix-run/dev
 
 export default createRequestHandler({ build });
