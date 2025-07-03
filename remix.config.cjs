d928e72d364587144881c2a339913ee21f3df866
Rich Cook <flaturtha@users.noreply.github.com>
2025-07-02 21:01:25 -0500

    Update and rename remix.config.mjs to remix.config.cjs

--- remix.config.cjs
+++ remix.config.cjs
@@ -1,14 +1,11 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   future: {
-    // enable static export
     unstable_staticExport: true,
   },
   routes: async (defineRoutes) =>
     defineRoutes((route) => {
-      // your index route
       route("/", "app/routes/_index.tsx");
-      // dynamic slug route (file: app/routes/$variant.tsx)
       route("/:variant", "app/routes/$variant.tsx");
     }),
 };
