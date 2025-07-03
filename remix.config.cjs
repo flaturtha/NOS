cb82df585907cbe4d8ac9d620d222522d662aa82
Rich Cook <flaturtha@users.noreply.github.com>
2025-07-02 19:00:34 -0500

    Delete remix.config.cjs

--- remix.config.cjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('@remix-run/dev').AppConfig} */
-module.exports = {
-  server: "./server.js",
-  serverBuildDirectory: ".netlify/functions-internal",
-  serverModuleFormat: "esm",
-  ignoredRouteFiles: ["**/.*"],
-}; 
