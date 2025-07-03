0b14bc936c68011015edb7400cb704b704d4c8f7
Rich Cook <flaturtha@users.noreply.github.com>
2025-07-02 19:38:29 -0500

    Update _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,7 +2,7 @@ export default function Index() {
   return (
     <div style={{ padding: "2rem" }}>
       <h1>Welcome to NOS</h1>
-      <p>This is the index route for your Remix landing page builder.</p>
+      <p>This is the index route for your Remix landing page builder!!!</p>
     </div>
   );
 }
